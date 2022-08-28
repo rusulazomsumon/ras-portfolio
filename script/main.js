2aac81910506cc3ee12deff1d13d2e624a36fdb7
RusulAzomSumon <18103020@iubat.edu>
2022-08-28 23:49:32 +0600

    js update

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 //********************* */ Back to Top Button ****************
 // Selection the button by class name
 const backToTop = document.querySelector(".back-to-top");
-// Adding conditon and propertics using arrow function 
+// Adding conditon and propertics using arrow function by the help of even listener 
 window.addEventListener("scroll", () => {
     if(window.pageYOffset>100){
         backToTop.classList.add("active");
